34ebf5d646217158e0d6e1db2054059cf077c625
tug48427 <tug48427@temple.edu>
2019-09-21 13:39:27 -0400

    Added a click behavior for save button created Toast messages for if the fields are empty or incorrect Toast message if user entered everything correctly once save was clicked

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -15,6 +15,51 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText uName = findViewById(R.id.userName);
+        final EditText email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.pass);
+        final EditText conPassword= findViewById(R.id.conPass);
+
+
+
+
+        Button clickSave = findViewById(R.id.saveButton);
+
+        clickSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getUName = uName.getText().toString();
+                String getEmail = email.getText().toString();
+                String getPass = password.getText().toString();
+                String getConPass = conPassword.getText().toString();
+                if (getUName.equals("")){
+                    Toast.makeText(FormActivity.this,"Missing Username",Toast.LENGTH_SHORT).show();
+                }
+                else if (getEmail.equals("")){
+                    Toast.makeText(FormActivity.this,"Missing Email",Toast.LENGTH_SHORT).show();
+                }else if (getPass.equals("")){
+                    Toast.makeText(FormActivity.this,"Missing Password",Toast.LENGTH_SHORT).show();
+                }else if (getConPass.equals("")){
+                    Toast.makeText(FormActivity.this,"Missing Confirm Password",Toast.LENGTH_SHORT).show();
+                }
+
+
+                else if(getPass.equals(getConPass)==false){
+                    Toast.makeText(FormActivity.this,"Missing Confirm Password",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(FormActivity.this,"Welcome, "+getUName+", to the SignUpForm App"
+                            ,Toast.LENGTH_SHORT).show();
+
+                }
+
+
+
+
+            }
+
+
+        });
 
 
 
