b1ee9c1fca1f8511d5ff54d2f4168e75b38289cf
tug48427 <tug48427@temple.edu>
2019-09-21 13:12:04 -0400

    Initial commit created and organized the layout for the sign up form added a button

---
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -0,0 +1,25 @@
+package edu.temple.signupform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+}
